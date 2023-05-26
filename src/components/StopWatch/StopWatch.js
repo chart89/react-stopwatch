6d42542e3c2c91c7a626e3c5569329c64b8b64c1
Darek Biernat <kaiju83@outlook.com>
2023-05-26 23:19:03 +0200

    Initial commit

---
+++ src/components/StopWatch/StopWatch.js
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
+const StopWatch = () => {
+    
+    const [time, setTime] = useState(0);
+    const [start, setStart] = useState(false);
+    const [pause, setPause] = useState(true);
+
+    useEffect(() => {
+        let timer = null; 
+    
+        if (start && pause === false){
+            timer = setInterval(() => {
+                setTime(time => time + 1);
+            }, 1);
+        } else {
+            clearInterval(timer);
+        };
+        return () => {
+            clearInterval(timer)
+        }
+    }, [start, pause, time]);
+
+    //method to start time
+    const timeStart = () => {
+        setStart(true);
+        setPause(false);
+    }
+
+    //method to pause time
+    const timePause = () => {
+        setPause(true);
+    }
+
+    //method to reset time using RESET button or when the state is remove
+    const timeReset = () => {
+        setStart(false);
+        setTime(0);
+    }
+
+    /* convert milliseconds to H:M:S - based on Internet */
+    const milliseconds = time % 100;
+    
+    const seconds = Math.floor((time / 1000) % 60);
+
+    const minutes = Math.floor((time / 1000 / 60) % 60);
+
+    const hours = Math.floor((time / 1000 / 60 / 60) % 24);
+   
+    return (
+         <div>{hours.toString().padStart(2, "0")}:{minutes.toString().padStart(2, "0")}:{seconds.toString().padStart(2, "0")}:{milliseconds.toString().padStart(2, "0")}
+         <button onClick={() => timeStart()}>START</button>
+         <button onClick={() => timePause()}>PAUSE</button>
+         <button onClick={() => timeReset()}>RESET</button>
+         </div>
+         
+    );
+};
+
+export default StopWatch;
